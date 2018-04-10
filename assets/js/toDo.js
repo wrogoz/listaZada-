d3dac60c27ba379da607615c1ec7c11c827e4b25
wrogoz <wrogoz@gmail.com>
2018-04-10 23:02:01 +0200

    dodano funkcje: usuwanie zadania

--- assets/js/toDo.js
+++ assets/js/toDo.js
@@ -1,3 +1,12 @@
+// skreślenie zadania po kliknięciu
 $("li").click(function(){
-$(this).toggleClass("completed");
+	$(this).toggleClass("completed");
+});
+
+// usuniecie zadania po kliknieciu x
+$("span").click(function(event){
+	$(this).parent().fadeOut(500,function(){
+	$(this).remove();
+});
+event.stopPropagation();
 });
