ab6f0fbfc939bea20eba71c409c0b861bebfcb42
wrogoz <wrogoz@gmail.com>
2018-04-12 23:11:49 +0200

    wykonano funkcjonalnosc strony, css

--- assets/js/toDo.js
+++ assets/js/toDo.js
@@ -1,12 +1,31 @@
 // skreślenie zadania po kliknięciu
-$("li").click(function(){
+$("ul").on("click","li", function(){  
 	$(this).toggleClass("completed");
 });
 
 // usuniecie zadania po kliknieciu x
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
 	$(this).parent().fadeOut(500,function(){
 	$(this).remove();
 });
 event.stopPropagation();
+});
+
+// dodanie zadania po wpisaniu tekstu
+
+$("input[type='text']").keypress(function(event){
+	if(event.which===13){   // cyfra oznaczająca enter
+	
+		var toDoText=$(this).val();	// pobiera wartosc z input
+		$(this).val("");		//wyczyszczenie inputa
+				// utworzenie nowego li	
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> "+toDoText+ "</li>")	;		
+				
+	}
+});
+
+
+$(".fa-plus").click(function(){
+
+	$("input[type='text']").fadeToggle();
 });
